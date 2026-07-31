28c179373d13c4f55ff9587fc41cc15526f1a3f9
Ashwin Naren <arihant2math@gmail.com>
2026-07-31 16:59:56 -0700

    fix

--- judge/__init__.py
+++ judge/__init__.py
@@ -1,13 +1,21 @@
 from litellm import completion
 from pydantic import BaseModel
 
-from access import Accessor
+from access import Accessor, DirectoryAccessor
 
-SYSTEM_PROMPT = "You are part of a pipeline meant to detect AI generated code."
+SYSTEM_PROMPT = """You are part of a pipeline meant to detect AI generated code.
+Return false for both is_ai and is_human if you are not sure.
+Return the files most likely to be AI generated in `suspicious_files`.
+
+Add any relevant comments in comments.
+"""
 
 
 class JudgeOutput(BaseModel):
-    pass
+    is_ai: bool
+    is_human: bool
+    suspicious_files: list[str]
+    comments: str
 
 
 def check_repo(accessor: Accessor):
@@ -17,6 +25,8 @@ def check_repo(accessor: Accessor):
             {"role": "system", "content": SYSTEM_PROMPT},
             {"role": "user", "content": "Hello, how are you?"},
         ],
+        response_format=JudgeOutput
     )
-
     print(response)
+
+check_repo(DirectoryAccessor("../winbond_flash_driver"))
