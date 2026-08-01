e69e25bb4b1d48aa7845f56a021e2c4b7b248f8e
Ashwin Naren <arihant2math@gmail.com>
2026-07-31 17:23:11 -0700

    nice printing

--- pipeline.py
+++ pipeline.py
@@ -9,7 +9,18 @@ if __name__ == "__main__":
     path = Path("../prontum")
     result = check_repo(DirectoryAccessor(path))
     print(result.model_dump_json(indent=2))
+    ai = []
+    prob_ai = []
+    certain_threshold = 0.75
     for file in result.key_files:
         code = open(path / file).read()
-        print(service.classify(code, file))
+        result = service.classify(code, file)
+        if result['ai_probability'] > certain_threshold:
+            ai.append(result)
+        if result['ai_probability'] > result['threshold']:
+            prob_ai.append(result)
+    print(f"{len(ai)} out of {len(result.key_files)} key files are certainly AI generated, {len(prob_ai)} are probably AI generated or assisted in some way")
+    print("Worst offenders")
+    for file in sorted(ai, key=lambda result: result["ai_probability"]):
+        print(f"- {file["input"]}: {file["ai_probability"]}")
 
