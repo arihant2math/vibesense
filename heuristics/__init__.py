bc623c8e9ad7733a0b5bc7a7280be363e049f38a
Ashwin Naren <arihant2math@gmail.com>
2026-07-31 20:53:05 -0700

    remove server, must rrir

---
+++ heuristics/__init__.py
@@ -0,0 +1,5 @@
+# TODO: Em-dashes: 25.90 versus 0.275 per million characters
+# TODO: look at coauthorship
+# - requires git
+# TODO: rate of LOC addition (https://pscanf.com/s/352/)
+# - also requires git
