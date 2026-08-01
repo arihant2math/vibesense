42f162839b0c1c7378df9b7fb65b6f05802bba49
Ashwin Naren <arihant2math@gmail.com>
2026-07-31 21:34:26 -0700

    remove python code

--- heuristics/__init__.py
+++
@@ -1,5 +0,0 @@
-# TODO: Em-dashes: 25.90 versus 0.275 per million characters
-# TODO: look at coauthorship
-# - requires git
-# TODO: rate of LOC addition (https://pscanf.com/s/352/)
-# - also requires git
